0c167761629cab0b4a2be22ffd8be7cec88cf9b3
jensbaetens <jens.baetens@ikdoeinformatica.be>
2023-11-28 19:09:38 +0100

    start les 1

---
+++ Week 1/index.js
@@ -0,0 +1,29 @@
+console.log('hello" world');
+
+// variabelen
+const constante = 5;
+let variabele;
+console.log(variabele);
+
+// const is constant, waarde kan niet meer aangepast worden
+//constante = variabele;
+// dit gaat wel want het is een variabele
+variabele = constante;
+// dit willen we vermijden, gebruik dit niet tenzij het niet anders kan
+var oudereVariabele = 10;
+
+variabele ++;
+variabele --;
+variabele = 10+5;
+variabele = 10* 5;
+variabele *= 5;   // variabele = variabele * 5
+
+variabele = '10';
+let variabele2 = 5;
+
+variabele2 = variabele + variabele2;
+
+let ditIsEenVoorbeeldVanGoedeNaamgeving;
+
+console.log(variabele);
+console.log(variabele2);
